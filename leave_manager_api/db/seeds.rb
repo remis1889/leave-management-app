bcff0017fc03e60d570868cc0aaf37efde587922
Remi S <remis1889@gmail.com>
2020-01-25 09:46:29 -0800

    seed db

--- leave_manager_api/db/seeds.rb
+++ leave_manager_api/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Destroying existing data...'
+
+Employee.destroy_all
+LeaveApplication.destroy_all
+
+puts 'Cleared existing data.'
+
+COUNT = 100
+MANAGER_COUNT = 5
+
+# Create employees
+puts 'Creating employees...'
+MANAGER_COUNT.times do
+  Employee.create(employee_name: Faker::Name.name, email: Faker::Internet.email, leave_balance: rand(31))
+end
+
+managers = Employee.pluck(:id)
+
+(COUNT - MANAGER_COUNT).times do
+    Employee.create(employee_name: Faker::Name.name, email: Faker::Internet.email, leave_balance: rand(31), manager_id: managers.sample)
+  end
+  
+
+puts "#{COUNT} employees created."
+
+puts 'Seeding Completed!'

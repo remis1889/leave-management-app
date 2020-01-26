e515a31bd76bb62a05fae5922c1bec897378c7d9
Remi S <remis1889@gmail.com>
2020-01-26 08:01:57 -0800

    add routes

--- leave_manager_api/config/routes.rb
+++ leave_manager_api/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :employees, only: :show do
+  resources :employees, only: [:show, :index] do
+    member do
+      get 'new_requests'
+    end
     resources :leave_applications, except: :show
   end  
 end

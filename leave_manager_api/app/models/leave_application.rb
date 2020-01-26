23bf4dbd21bd67485a3adcc20d9e12b04d9cb1aa
Remi S <remis1889@gmail.com>
2020-01-26 07:58:41 -0800

    add helper methods in models

--- leave_manager_api/app/models/leave_application.rb
+++ leave_manager_api/app/models/leave_application.rb
@@ -1,4 +1,23 @@
 class LeaveApplication < ApplicationRecord
     belongs_to :employee
-    validates :from, :to, presence: true    
+    validates :from, :to, presence: true  
+    after_update :reduce_leave_balance, if: :leave_application_approved?
+
+    PENDING = 0
+    APPROVED = 1
+    DECLINED = 2
+    
+    def total_days
+        (to - from).to_i
+    end
+
+    def leave_application_approved?
+        status == APPROVED
+    end
+
+    def reduce_leave_balance        
+        current_balance = employee.leave_balance - total_days
+        employee.update(leave_balance: current_balance)
+    end
+
 end
